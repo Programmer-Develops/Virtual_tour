7a809f4196d4731feee57fc8bd241dcc5e83a8a1
Programmer-Develops <shachipandya29@gmail.com>
2025-11-13 11:56:49 +0530

    added sidebar, logo and improved cursor

--- js/Tour.js
+++ js/Tour.js
@@ -2,32 +2,46 @@ AFRAME.registerComponent("tour", {
   schema: {
     state: { type: "string", default: "places-list" },
     selectedCard: { type: "string", default: "#card1" },
-    zoomAspectRatio: { type: "number", default: 1 }
+    zoomAspectRatio: { type: "number", default: 1 },
   },
-  init: function() {
+  init: function () {
     this.placesContainer = this.el;
     this.cameraEl = document.querySelector("#camera");
     this.createCards();
   },
-  tick: function() {
+  tick: function () {
     const { state } = this.el.getAttribute("tour");
 
     if (state === "view") {
       this.hideEl([this.placesContainer]);
       this.showView();
+
+      const sidebar = document.querySelector("#sidebar");
+      const sidebarTitle = document.querySelector("#sidebar-title");
+      const sidebarBody = document.querySelector("#sidebar-body");
+
+      sidebarTitle.setAttribute("text", { value: "Taj Mahal" });
+      sidebarBody.setAttribute("text", {
+        value:
+          "Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan (ruler. 1628–1658) to house the tomb of his favourite wife, Mumtaz Mahal.",
+      });
+
+      sidebar.setAttribute("visible", true);
+      sidebarTitle.setAttribute("visible", true);
+      sidebarBody.setAttribute("visible", true);
     }
   },
-  hideEl: function(elList) {
-    elList.map(el => {
+  hideEl: function (elList) {
+    elList.map((el) => {
       el.setAttribute("visible", false);
     });
   },
-  createCards: function() {
+  createCards: function () {
     const thumbNailsRef = [
       {
         id: "taj-mahal",
         title: "Taj Mahal",
-        url: "../assets/thumbnails/taj_mahal.png"
+        url: "../assets/thumbnails/taj_mahal.png",
       },
       // {
       //   id: "budapest",
@@ -64,42 +78,42 @@ AFRAME.registerComponent("tour", {
       this.placesContainer.appendChild(borderEl);
     }
   },
-  createBorder: function(position, id) {
+  createBorder: function (position, id) {
     const entityEl = document.createElement("a-entity");
     entityEl.setAttribute("id", id);
     entityEl.setAttribute("visible", true);
     entityEl.setAttribute("geometry", {
       primitive: "ring",
       radiusInner: 9,
-      radiusOuter: 10
+      radiusOuter: 10,
     });
     entityEl.setAttribute("position", position);
     entityEl.setAttribute("material", {
       color: "rgba(0, 0, 0, 1)",
-      opacity: 0.4
+      opacity: 0.4,
     });
     entityEl.setAttribute("cursor-listener", {});
     return entityEl;
   },
-  createThumbNail: function(item) {
+  createThumbNail: function (item) {
     const entityEl = document.createElement("a-entity");
     entityEl.setAttribute("visible", true);
     entityEl.setAttribute("geometry", {
       primitive: "circle",
-      radius: 9
+      radius: 9,
     });
     entityEl.setAttribute("material", { src: item.url });
     entityEl.setAttribute("cursor-listener", {});
     return entityEl;
   },
-  createTitleEl: function(position, item) {
+  createTitleEl: function (position, item) {
     const entityEl = document.createElement("a-entity");
     entityEl.setAttribute("text", {
       font: "exo2bold",
       align: "center",
       width: 80,
       color: "rgba(7, 70, 158, 1)",
-      value: item.title
+      value: item.title,
     });
     const elPosition = position;
     elPosition.y = -20;
@@ -107,16 +121,16 @@ AFRAME.registerComponent("tour", {
     entityEl.setAttribute("visible", true);
     return entityEl;
   },
-  showView: function() {
+  showView: function () {
     const { selectedCard } = this.data;
     const skyEl = document.querySelector("#main-container");
     skyEl.setAttribute("material", {
       src: `../assets/360_images/${selectedCard}/place-0.jpg`,
-      color: "#fff"
+      color: "#fff",
     });
   },
-  update: function() {
-    window.addEventListener("keydown", e => {
+  update: function () {
+    window.addEventListener("keydown", (e) => {
       if (e.key === "ArrowUp") {
         if (
           (this.data.zoomAspectRatio <= 10 && this.data.state === "view") ||
@@ -136,5 +150,5 @@ AFRAME.registerComponent("tour", {
         }
       }
     });
-  }
+  },
 });
